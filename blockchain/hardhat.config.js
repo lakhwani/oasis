56e0f5c7229f42d0097d049729ca6b8d42f7ffd0
SabrinaHirani <sabrinahirani@gmail.com>
2024-06-21 20:02:20 -0400

    Set config back to sepolia

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -6,16 +6,15 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    hardhat: {},
-    // sepolia: {
-    //   chainId: 11155111,
-    //   url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID,
-    //   accounts: {
-    //     mnemonic: process.env.MNEMONIC,
-    //     path: "m/44'/60'/0'/0",
-    //     initialIndex: 0,
-    //     count: 20,
-    //   },
-    // },
+    sepolia: {
+      chainId: 11155111,
+      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+    },
   },
 };
